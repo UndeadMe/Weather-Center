3a64a1dd19b294f387f3f5541f8e4ee324d7a4d1
Banana021s <arash.k021@yahoo.com>
2022-01-11 20:32:05 +0330

    handle errors in ask location page

--- public/content/js/ask-location.js
+++ public/content/js/ask-location.js
@@ -5,32 +5,39 @@ const gpsMsg = document.querySelector(".gps-msg")
 //* request to user for allow location 
 const geolocationReq = () => {
     fetch("https://api.ipify.org/?format=json")
-            .then(result => result.json())
-            .then(data => { 
-                return fetch(`https://geocode.xyz?auth=629829819389156476277x31205&locate=${data.ip}&geoit=json`)
-            })
-            .then(res => res.json())
-            .then(data => {
-                //* set geolocation of user in localStorage
-                localStorage.setItem("Geolocation", JSON.stringify({
-                    country: data.country,
-                    city: data.city,
-                    latt: data.latt,
-                    longt: data.longt
-                }))
-                //* go to the home page and send the geo info
-                location.replace(`home.html`)
-            })
-            .catch(err => {
-                //* stop loading
-                loading(false)
-                console.error(err)
-            })
+        .then(result => result.json())
+        .then(data => { 
+            return fetch(`https://geocode.xyz?auth=629829819389156476277x31205&locate=${data.ip}&geoit=json`)
+        })
+        .then(res => res.json())
+        .then(data => {
+            //* set geolocation of user in localStorage
+            localStorage.setItem("Geolocation", JSON.stringify({
+                country: data.country,
+                city: data.city,
+                latt: data.latt,
+                longt: data.longt
+            }))
+            //* go to the home page and send the geo info
+            location.replace(`home.html`)
+        })
+        .catch(err => {
+            //* stop loading
+            loading(false)
+            //* render error
+            renderErr(`${err.message} <br> couldn't get access`)
+        })
 }
 
 //* start loading or stop loading
 const loading = (bool) => {
     bool ? loadingElem.classList.add("active") : loadingElem.classList.remove("active")
+}
+
+//* render errors
+const renderErr = (err) => {
+    gpsMsg.classList.add("gps-msg-err")
+    gpsMsg.innerHTML = err
 }
 
 locationBtn.addEventListener("click", () => {
